1be89abd0a97ec90f098c4f82ed0edf670f5b907
Alvaro-gonzalez05 <128165993+Alvaro-gonzalez05@users.noreply.github.com>
2024-05-10 11:29:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import { createPool } from "mysql2/promise"
+import {config} from "dotenv";
+config()
+
+const app = express()
+
+
+const pool = createPool ({
+    host: process.env.MYSQLDB_HOST,
+    user: "root",
+    password: process.env.MYSQLDB_ROOT_PASSWORD,
+    port: process.env.MYSQLDB_DOCKER_PORT
+})
+
+app.get ("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.get ("/ping", async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    res.json(result[0])
+})
+
+app.listen(3000);
+console.log("Server on port", 3000);
